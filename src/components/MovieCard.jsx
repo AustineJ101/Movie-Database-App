ce6e83e6ebe949ad604a5c37f1250bf70c80b23a
Austine Juma <austinejumah001@gmail.com>
2025-03-27 05:20:28 +0300

    feat: Navigate to details page onClick

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
-import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 
-function MovieCard({poster_path, title, date}){
+function MovieCard({poster_path, title, date, id}){    
     const imageBaseUrl = "https://image.tmdb.org/t/p/w200"
+
+    const navigate = useNavigate();
+
     return(
-        <>
-        <SearchBar />
-        <div className="flex flex-col gap-1 w-[200px] hover:scale-105 transition  rounded-xl border cursor-pointer ">
-            <img className="rounded-xl" src={imageBaseUrl+poster_path} alt={title} />
+        <div onClick={() => navigate(`/details/${id}`)} className="flex flex-col gap-1 w-[170px] hover:scale-105 transition  rounded-xl border cursor-pointer ">
+            <img className="rounded-xl flex-1" src={imageBaseUrl+poster_path} alt={title} />
             <h1 className="truncate text-xl font-semibold ml-1">{title}</h1>
             <p className="m-1 text-gray-500">{date}</p>
         </div>
-        </>
+
        
     )
 }
