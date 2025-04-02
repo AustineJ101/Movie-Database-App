e2653fd685ba0c5636339e58aa1ef6e27747677d
Austine Juma <austinejumah001@gmail.com>
2025-04-02 16:29:19 +0300

    cast card style: truncate names

--- src/components/CastCard.jsx
+++ src/components/CastCard.jsx
@@ -8,7 +8,7 @@ function CastCard({profile_path, name, character}){
         <div className="flex flex-col gap-1 gap-1 min-w-[100px] md:min-w-[150px] xl:min-w-[170px]  hover:scale-105 transition  rounded-xl border cursor-pointer ">
             <img className="rounded-xl flex-1" src={profileImg} alt={character} />
             <h1 className="truncate text-sm md:text-lg xl:text-xl font-semibold ml-1">{name}</h1>
-            <p className="m-1 text-gray-500 text-sm md:text-lg xl:text-xl">{character}</p>
+            <p className="truncate m-1 text-gray-500 text-sm md:text-lg xl:text-xl">{character}</p>
         </div>
 
        
